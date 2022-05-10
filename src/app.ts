a2c2ea578dbd873df72ec075e6301288e12e7236
alfonsus20 <alfonschandrawan@gmail.com>
2022-05-10 23:58:12 +0700

    create user model

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { PORT } from "./config";
-import cors from 'cors';
+import cors from "cors";
+import { connectDatabase } from "./utils/db";
+import User from "./models/user.model";
 
 const app = express();
 
@@ -8,9 +10,28 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/', (req, res)=>{
-  res.json({message : "Hai"})
-})
+connectDatabase();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hai" });
+});
+
+app.post("/halo", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      name: "Alfons",
+      email: "alfonschandrawan@gmail.com",
+      password: "123",
+      entryYear: 2019,
+      majority: "Informatika",
+      isAdmin: true,
+    });
+    console.log(newUser);
+    res.json({ message: "mantap" });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.listen(PORT || 3000, () => {
   console.log(`Server started on port ${PORT || 3000}`);
---
+++ src/models/user.model.ts
@@ -0,0 +1,74 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+import sequelize from "../config/db";
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id: CreationOptional<number>;
+  declare name: string;
+  declare email: string;
+  declare password: string;
+  declare majority: string;
+  declare entryYear: number;
+  declare graduationYear?: number;
+  declare thesisURL?: string;
+  declare profileImageURL?: string;
+  declare isAdmin?: boolean;
+  declare isVerified?: boolean;
+
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    majority: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    entryYear: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    graduationYear: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    thesisURL: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    profileImageURL: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    isAdmin: { type: DataTypes.BOOLEAN, defaultValue: false },
+    isVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  { sequelize, tableName: "users" }
+);
+
+export default User;
