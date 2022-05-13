09451f9a6cff5616003238c4720dad99e6a0e408
alfonsus20 <alfonschandrawan@gmail.com>
2022-05-13 19:05:12 +0700

    add next params

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { HttpException } from "../exceptions/HttpException";
 
-export default (error: HttpException, _: Request, res: Response) => {
+export default (error: HttpException, _: Request, res: Response, next:NextFunction) => {
   const statusCode = error.status || 500;
   res.status(statusCode).json({ message: error.message, data: error.data });
 };
