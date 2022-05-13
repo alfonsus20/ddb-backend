de6c07935e1f384c9c3db24163a4e78e30619da5
alfonsus20 <alfonschandrawan@gmail.com>
2022-05-13 10:23:56 +0700

    remove dist

--- dist/middlewares/error.middleware.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (error, _, res) => {
-    const statusCode = error.status || 500;
-    res.status(statusCode).json({ message: error.message, data: error.data });
-};
